04be83114de4d7fb14d5148542796717245e27b1
yujinpan <1192878390@qq.com>
2024-04-15 17:16:05 +0800

    test: update method params

--- src/service.spec.ts
+++ src/service.spec.ts
@@ -2,8 +2,8 @@ import { getV2RaySubResult } from './service';
 
 describe('Service', () => {
   it('should get sub correct', () => {
-    expect(getV2RaySubResult('1.1.1.1')).toEqual(
-      `c3M6Ly9ZV1Z6TFRJMU5pMW5ZMjA2TVRJek5EVTJRREV1TVM0eExqRTZNVEV4TVE9PSN0ZXN0CnZtZXNzOi8vZXlKd2N5STZJblJsYzNRaUxDSndiM0owSWpveE1URXhMQ0pwWkNJNklqRXlNelExTmlJc0ltRmtaQ0k2SWpFdU1TNHhMakVpZlE9PQ==`,
+    expect(getV2RaySubResult()).toEqual(
+      `c3M6Ly9ZV1Z6TFRJMU5pMW5ZMjA2TVRJek5EVTJRREUyT1M0eU5UUXVNak0yTGpZek9qRXhNVEU9I3Rlc3QKdm1lc3M6Ly9leUp3Y3lJNkluUmxjM1FpTENKd2IzSjBJam94TVRFeExDSnBaQ0k2SWpFeU16UTFOaUlzSW1Ga1pDSTZJakUyT1M0eU5UUXVNak0yTGpZekluMD0=`,
     );
   });
 });
